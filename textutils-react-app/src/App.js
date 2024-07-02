9faa66ba6cfe674a09fa9394c255faf0d3569cec
akanksh <ee18b002@iittp.ac.in>
2024-07-02 17:16:54 +0530

    code changes till 8th video in the playlist

--- textutils-react-app/src/App.js
+++ textutils-react-app/src/App.js
@@ -3,10 +3,8 @@ import TextForm from "./components/TextForm";
 function App() {
   return (
     <>
-    <Navbar title="TextUtils" aboutText="About TextUtils" />
-    <div className="container">
-      <TextForm heading="Enter the text to analyze below"/>
-    </div>
+      <Navbar title="TextUtils" aboutText="About TextUtils" />
+      <TextForm heading="Enter the text to analyze below" />
     </>
   );
 }
--- textutils-react-app/src/components/TextForm.js
+++ textutils-react-app/src/components/TextForm.js
@@ -14,11 +14,22 @@ export default function TextForm(props) {
   }
   const [text,setText] = useState("This is the default Text");
   return (
-    <div>
+    <>
+    <div className='container'>
       <h1>{props.heading}</h1>
       <textarea className="form-control" rows="8" id="myBox" value={text} onChange={handleOnChange}></textarea>
       <button className='btn btn-primary m-1' onClick={handleUpClick} >Convert to UpperCase</button>
       <button className='btn btn-primary m-1' onClick={handleLowerClick} >Convert to LowerCase</button>
     </div>
+
+
+    <div className="container my-3">
+      <h1>Your text summary</h1>
+      <p>{text.split(" ").length} words and {text.length} characters</p>
+      <p>{0.008 * text.split(" ").length} Minutes read</p>
+      <h2>Preview</h2>
+      <p>{text}</p>
+    </div>
+    </>
   )
 }
